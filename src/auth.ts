9b86b6f5689f6b6d9980152827ec88d14cbf5d89
Ramon Adofo <ramon.adofo_ext@zooplus.com>
2023-07-10 09:04:51 +0200

    feat: authentication and redirects

--- src/auth.ts
+++ src/auth.ts
@@ -1,30 +1,7 @@
-import { supabase } from './supabaseClient';
+import type { SupabaseClient } from '@supabase/supabase-js';
 import type { InsertCashGroupDTO } from './supabaseTypes';
 
-export async function getSupabaseUser() {
-	const {
-		data: { user }
-	} = await supabase.auth.getUser();
-
-	return user;
-}
-
-export async function loginUser(email: string, password: string) {
-	const { data, error } = await supabase.auth.signInWithPassword({
-		email,
-		password
-	});
-
-	return { data, error };
-}
-
-export async function logoutUser() {
-	const { error } = await supabase.auth.signOut();
-
-	return error;
-}
-
-export async function insertCashGroup(dto: InsertCashGroupDTO) {
+export async function insertCashGroup(dto: InsertCashGroupDTO, supabase: SupabaseClient) {
 	const { data, error } = await supabase.from('cash_group').insert([dto]).select();
 	console.log('error:', error);
 	console.log('data:', data);
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,49 @@
-<script>
-	import '../app.css';
+<script lang="ts">
+	import { invalidate } from '$app/navigation';
+	import { onMount } from 'svelte';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+
+	let { supabase, session } = data;
+	$: ({ supabase, session } = data);
+
+	let email: string;
+	let password: string;
+
+	onMount(() => {
+		const {
+			data: { subscription }
+		} = supabase.auth.onAuthStateChange((event, _session) => {
+			if (_session?.expires_at !== session?.expires_at) {
+				invalidate('supabase:auth');
+			}
+		});
+
+		return () => subscription.unsubscribe();
+	});
+
+	const handleSignIn = async () => {
+		await supabase.auth.signInWithPassword({
+			email,
+			password
+		});
+	};
+
+	const handleSignOut = async () => {
+		await supabase.auth.signOut();
+	};
 </script>
 
-<slot />
+{#if session}
+	<button on:click={handleSignOut}>Sign out</button>
+	<a href="/cash-group">Cash Groups</a>
+	<a href="/">Home</a>
+	<slot />
+{:else}
+	<p>Please Login</p>
+	<input name="email" bind:value={email} />
+	<input type="password" name="password" bind:value={password} />
+
+	<button on:click={handleSignIn}>Sign in</button>
+{/if}
