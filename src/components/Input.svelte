1d224336df0d07f4deb5b5bc65e0ab84c2c56023
Ramon Adofo <ramon.adofo_ext@zooplus.com>
2024-01-28 15:41:34 +0100

    fix: revert toggle password visibility

--- src/components/Input.svelte
+++ src/components/Input.svelte
@@ -1,13 +1,10 @@
 <script lang="ts">
-	import { IconEye, IconEyeOff  } from "@tabler/icons-svelte";
-	import Button from "./Button.svelte";
 
 	export let inputValue: string;
 	export let inputType: 'text' | 'number' | 'date' | 'password';
 	export let textAlign: 'text-right' | 'text-left' = 'text-left';
 	export let hint: string = '';
 
-  let showPassword: boolean = false;
 </script>
 
 {#if inputType === 'text'}
@@ -32,25 +29,9 @@
 		bind:value={inputValue}
 	/>
 {:else if inputType === 'password'}
-  <div class="flex items-center gap-1 w-full shrink">
-      {#if showPassword}
-        <input
-          type="text"
-          class="rounded-lg border p-2 pl-3 flex-1 {textAlign}"
-          bind:value={inputValue}
-        />
-        <button on:click={() => showPassword = !showPassword}>
-            <IconEyeOff class="min-w-fit pl-1 text-slate-500" size={24}/>
-        </button>
-      {:else}
         <input
           type="password"
-          class="shrink rounded-lg border flex-1 p-2 pl-3 {textAlign}"
+          class="w-full shrink rounded-lg border p-2 pl-3 {textAlign}"
           bind:value={inputValue}
         />
-        <button class="shrink" on:click={() => showPassword = !showPassword}>
-            <IconEye class="min-w-fit pl-1 text-slate-500" size={24} />
-        </button>
-      {/if}
-  </div>
 {/if}
