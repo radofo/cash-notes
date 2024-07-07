12fbc0577b9877ddd56b2fed42ed616cc3a5d6f6
Ramon Adofo <ramon.adofo_ext@zooplus.com>
2024-07-07 11:42:52 +0200

    feat: spacing

--- src/components/Monthly/TotalView.svelte
+++ src/components/Monthly/TotalView.svelte
@@ -2,7 +2,7 @@
 	import TotalTable from '../Total/TotalTable.svelte';
 </script>
 
-<div class="flex flex-col gap-2">
+<div class="flex flex-col gap-3">
 	<span class="text-md block text-start font-bold">Geplante monatliche Finanzen</span>
 	<TotalTable />
 </div>
