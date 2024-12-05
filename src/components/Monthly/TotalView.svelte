7561afdb65f2f0b96c382a3b3fa737f5d2119b7e
Ramon Adofo <ramonadofo@hotmail.com>
2024-12-05 20:43:09 +0100

    feat: font size of header

--- src/components/Monthly/TotalView.svelte
+++ src/components/Monthly/TotalView.svelte
@@ -3,10 +3,10 @@
 	import TotalTable from '../Total/TotalTable.svelte';
 </script>
 
-<div class="flex w-full flex-col gap-8">
+<div class="flex w-full flex-col gap-10">
 	<div class="flex flex-row items-center justify-between">
-		<span class="block text-center text-lg font-bold">Geplante monatliche Zahlungen</span>
-		<a href="/budgets/edit" class="self-end">
+		<span class="block text-center text-2xl font-medium">Monatlich</span>
+		<a href="/budgets/edit" class="">
 			<Pen class="cursor-pointer" size={18} />
 		</a>
 	</div>
