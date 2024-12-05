364c5d409579df90dbb8b009120802c4af65e9fe
Ramon Adofo <ramonadofo@hotmail.com>
2024-12-05 20:36:02 +0100

    feat: more design updates

--- src/components/Monthly/TotalView.svelte
+++ src/components/Monthly/TotalView.svelte
@@ -4,11 +4,11 @@
 </script>
 
 <div class="flex w-full flex-col gap-8">
-	<div class="flex flex-row items-center justify-center">
+	<div class="flex flex-row items-center justify-between">
 		<span class="block text-center text-lg font-bold">Geplante monatliche Zahlungen</span>
+		<a href="/budgets/edit" class="self-end">
+			<Pen class="cursor-pointer" size={18} />
+		</a>
 	</div>
-	<a href="/budgets/edit" class="self-end">
-		<Pen class="cursor-pointer" size={18} />
-	</a>
 	<TotalTable />
 </div>
