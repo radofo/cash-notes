9b35a52905802943db2c5d7ee102ccb8f7f5e5c4
Ramon Adofo <ramonadofo@hotmail.com>
2024-12-06 10:48:01 +0100

    feat: reverse hightlights again

--- src/components/ProgressElement.svelte
+++ src/components/ProgressElement.svelte
@@ -22,9 +22,9 @@
 	<div class="relative h-2 w-full overflow-hidden rounded bg-slate-200">
 		<div class="absolute left-0 top-0 h-2 rounded bg-sky-600" style="width: {spendingProgress}%" />
 		<div
-			class="absolute bottom-0 top-0 rounded border {monthProgress > spendingProgress
+			class="absolute bottom-0 top-0 border {monthProgress < spendingProgress
 				? 'border-sky-900'
-				: 'border-slate-300'}"
+				: 'border-slate-400'}"
 			style="left: {monthProgress}%"
 		/>
 	</div>
