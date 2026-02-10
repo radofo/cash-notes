5faca97839bc46609017aa2860935956bd78b6d9
Ramon Adofo <raad@netlight.com>
2026-02-10 11:43:32 +0100

    feat: font changes

--- src/components/ProgressElement.svelte
+++ src/components/ProgressElement.svelte
@@ -54,10 +54,10 @@
 
 		<!-- Content inside the bar -->
 		<div
-			class="text-md relative z-20 flex flex-row items-center justify-between px-3 py-2.5 text-foreground/70"
+			class="relative z-20 flex flex-row items-center justify-between px-3 py-2.5 text-sm text-foreground/70"
 		>
-			<span class="font-normal">{name}</span>
-			<span class="font-normal tabular-nums">
+			<span class="font-medium">{name}</span>
+			<span class="font-medium tabular-nums">
 				{formatCurrency(Math.round(displayValue))}{info.limit !== null
 					? ` / ${displayCurrency({ amount: Math.round(info.limit), forceDecimals: false })}`
 					: ''}
